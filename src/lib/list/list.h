581e49c8b8413246987575918c31706a23d8f89b
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-02-18 18:04:28 +0100

    add interface

---
+++ src/lib/list/list.h
@@ -0,0 +1,29 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct List List;
+typedef struct ListIterator ListIterator;
+
+List *list_new();
+
+void list_destroy(List *list);
+
+int list_get_elements_count(const List *list);
+
+bool list_contains(const char *value, const List *list);
+
+int list_append(const char *value, List *list);
+
+int list_addall(List *source, List *destination);
+
+ListIterator *list_iterator_new(const List *list);
+
+void list_iterator_destroy(ListIterator *iterator);
+
+bool list_iterator_has_next(const ListIterator *iterator);
+
+void list_iterator_advance(ListIterator *iterator);
+
+char *list_iterator_get_element(const ListIterator *iterator);
+
+#endif
